218f4b699f1923d64958944e316ee2c0c81a029f
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-05 22:05:41 -0500

    BE: Added base Listings model [WIP]

--- server/listings/models.py
+++ server/listings/models.py
@@ -1,3 +1,20 @@
+"""Listing Model."""
 from django.db import models
 
-# Create your models here.
+
+class Listing(models.Model):
+    """Base Listing Model."""
+
+    company = models.CharField(max_length=50)
+    date_listed = models.DateField()
+    description = models.TextField()
+    languages = models.CharField(max_length=200)
+    location = models.CharField(max_length=25)
+    required_experience = models.CharField(max_length=25)
+    salary = models.CharField(max_length=7)
+    title = models.CharField(max_length=100)
+    url = models.TextField()
+    created_at = models.DateField()
+
+    def __str__(self):
+        return f"Listing(<id={self.id} title={self.title} company={self.company} />)"
