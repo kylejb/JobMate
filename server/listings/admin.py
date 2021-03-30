d98893c64d57363f8eb5f863ba5f802277b000c2
Matthew Leng <mleng89@gmail.com>
2021-03-30 17:02:54 -0400

    BE: Migrated the tables and listing site view

--- server/listings/admin.py
+++ server/listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing, FrontendFramework, BackendFramework, TechStack
+
+
+class ListingAdmin(admin.ModelAdmin):
+
+    # list_display = ("company",)
+    ...
+
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(TechStack, ListingAdmin)
+admin.site.register(FrontendFramework, ListingAdmin)
+admin.site.register(BackendFramework, ListingAdmin)
