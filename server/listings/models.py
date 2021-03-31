e11d1e68c9fd74edbccb6f5b346c67066c6e0c8e
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-30 20:07:32 -0400

    Reformatted dunder str's return statements

--- server/listings/models.py
+++ server/listings/models.py
@@ -6,14 +6,14 @@ class FrontendFramework(models.Model):
     framework = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.framework
+        return f"FrontendFramework({self.framework})"
 
 
 class BackendFramework(models.Model):
     framework = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.framework
+        return f"BackendFramework({self.framework})"
 
 
 class TechStack(models.Model):
@@ -21,9 +21,7 @@ class TechStack(models.Model):
     backend_framework = models.ForeignKey(BackendFramework, on_delete=models.CASCADE)
 
     def __str__(self):
-        return (
-            self.frontend_framework.framework + " " + self.backend_framework.framework
-        )
+        return f"TechStack({self.frontend_framework.framework} {self.backend_framework.framework})"
 
     # languages = JavaScript, Python
     # frontend_frameworks = ReactJS
