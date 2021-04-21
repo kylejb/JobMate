6c00c5f941310e3c18b20a7480911bdb8edc8c75
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-21 18:11:48 -0400

    BE: Added cors middleware

--- server/jobmate_api/settings.py
+++ server/jobmate_api/settings.py
@@ -35,6 +35,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # Application definition
 
@@ -45,8 +50,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "listings.apps.ListingsConfig",
     "rest_framework",
+    "corsheaders",
+    "listings.apps.ListingsConfig",
 ]
 
 # Use Django's standard `django.contrib.auth` permissions,
@@ -58,9 +64,10 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
