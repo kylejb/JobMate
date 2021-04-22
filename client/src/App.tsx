6224855e4a1fdf3feff1569c410bb3b19045fdfd
Matthew Leng <mleng89@gmail.com>
2021-04-22 15:20:56 -0400

    FE: Added Navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
-import Form from "components/Form";
-
+import Form from 'components/Form';
+import Navbar from 'components/Navbar';
 function App() {
-  return (
-    <div className="app">
-      <h1>App Component</h1>
-      <Form exampleProp="typeString" />
-    </div>
-  );
+	return (
+		<div className='app'>
+			<Navbar />
+			<h1>App Component</h1>
+			<Form exampleProp='typeString' />
+		</div>
+	);
 }
 
 export default App;
