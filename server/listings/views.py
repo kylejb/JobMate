a60f7849048dbc7708bd86f9cb32d1646cad96d0
Matthew Leng <mleng89@gmail.com>
2021-06-02 18:48:16 -0400

    BE: Added get_queryset method for search result

--- server/listings/views.py
+++ server/listings/views.py
@@ -6,3 +6,11 @@ from rest_framework import viewsets
 class ListingView(viewsets.ModelViewSet):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+
+    def get_queryset(self):
+        search_query = self.request.query_params.get("search", None)
+
+        if search_query is not None:
+            queryset = self.queryset.filter(title=search_query)
+
+        return queryset
