0a66da20fec48eb2968cf9798b45f682b64eb361
Matthew Leng <mleng89@gmail.com>
2021-06-03 19:17:08 -0400

    FE: Changed Name of file from 404 to NotFound

--- client/src/components/404/404.tsx
+++
@@ -1,9 +0,0 @@
-export default function notFound() {
-	return (
-		<div>
-			<h2>Error!</h2>
-			<h3>The page you are looking for does not exist.</h3>
-			<h3>Please click the top left to go back to the home page.</h3>
-		</div>
-	);
-}
